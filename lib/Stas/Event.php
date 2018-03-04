a1c7f2d3ae0815bbfb9efeaf359160926a984f7d
yaecho <dz1401@aliyun.com>
2018-03-04 08:51:34 +0800

    事件

---
+++ lib/Stas/Event.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * 简易的事件循环类
+ */
+namespace Stas;
+
+use stas\RunInterface;
+
+class Event
+{
+    /**
+     * 函数或可运行类
+     *
+     * @var array
+     * @author Yaecho 
+     */
+    private static $funcs = array();
+
+    /**
+     * 运作tag的索引
+     *
+     * @var array
+     * @author Yaecho 
+     */
+    private static $map = array();
+
+    /**
+     * 新增事件
+     *
+     * @param string $tag 标签
+     * @param  Closure|string $func 匿名函数或类名
+     * @return void
+     * @author Yaecho 
+     */
+    public static function add(string $tag, $func)
+    {
+        self::$funcs[$tag][] = $func;
+    }
+
+    /**
+     * 加载运作tag的索引
+     *
+     * @param array $map 运作tag的索引
+     * @return void
+     * @author Yaecho 
+     */
+    public static function loadMap(array $map)
+    {
+        static::$map = $map;
+    }
+
+    /**
+     * 事件循环
+     *
+     * @return void
+     * @author Yaecho 
+     */
+    public static function loop()
+    {
+        while (1) {
+            $count = count(static::$map);
+            for($i=0;$i<$count;$i++) {
+                self::trigger(static::$map[$i]);
+            }
+        }
+    }
+
+    /**
+     * 触发标签运行点
+     *
+     * @param string $tag 标签
+     * @return void
+     * @author Yaecho 
+     */
+    protected static function trigger(string $tag)
+    {
+        if (!array_key_exists($tag, self::$funcs)) {
+            return;
+        }
+        foreach (self::$funcs[$tag] as $func) {
+            // 如果是匿名函数直接调用
+            if (is_callable($func)) {
+                $func();
+            }
+            // 如果是字符，判断是不是类，是否实现接口
+            if (is_string($func) && class_exists($func)) {
+                $class = new $func;
+                if ($class instanceof RunInterface) {
+                    $class->run();
+                }
+            }
+        }
+    }
+
+}
---
+++ lib/Stas/RunInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 可运行事件接口
+ */
+namespace stas;
+
+interface RunInterface
+{
+    /**
+     * 运行
+     *
+     * @return void
+     * @author Yaecho 
+     */
+    public function run();
+}
