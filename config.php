607bab947acd1ba1fdad5149b5b1f75e3eadf0a4
yaecho <dz1401@aliyun.com>
2018-03-06 14:43:29 +0800

    add config

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+    'db' => array(
+        'database_type' => 'mysql',
+        'database_name' => 'STASdata',
+        'server' => 'localhost',
+        'username' => 'root',
+        'password' => 'root',
+
+        // [optional]
+        'charset' => 'utf8',
+        'port' => 3306,
+
+        // [optional] Table prefix
+        'prefix' => 'PREFIX_',
+    )
+);
